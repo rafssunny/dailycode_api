28f77942759070189328752230de30f761e1e922
Rafael Olegário <rafaelolegario2008@gmail.com>
2026-03-11 11:45:45 -0300

    fix OPTIONS method error

--- api/index.php
+++ api/index.php
@@ -2,8 +2,16 @@
 
 use Rafa\DailycodeApi\Router;
 use Rafa\DailycodeApi\src\middlewares\ApiKey;
-header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type, API-KEY");
+header("Access-Control-Allow-Methods: GET, OPTIONS");
 header('Content-type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 date_default_timezone_set('UTC');
 
 include_once __DIR__ . '/../vendor/autoload.php';
